f638d50ac3aa37448e70c993495e8927aa47c76c
Rıdvan Güzel <ridvanguzel42@gmail.com>
2025-04-03 20:13:12 +0300

    Karakter sayaç projesi yapıldı

---
+++ karakter-sayaci/karaktersayaci.js
@@ -0,0 +1,15 @@
+const textarea = document.querySelector('#karakterTextArea');
+const totalCounter = document.querySelector('#totalCounter');
+const remainingCounter = document.querySelector('#remainingCounter')
+
+textarea.addEventListener('keyup', updateCounter);
+function updateCounter(){
+    totalCounter.textContent = textarea.value.length;
+    remainingCounter.textContent = (textarea.getAttribute('MaxLength') - textarea.value.length);
+    if(remainingCounter.textContent == 0){
+        
+        setTimeout(() => {
+            alert('Karakter sayısı bitti agam');
+        }, 2000);
+    }
+}
